be5aa3262a70911583bf091e090e6d70744e1cc1
cikezmedia <chikezievictorc@gmail.com>
2023-07-19 18:14:03 +0100

    updated the porfolio api

--- src/app/api/portfolio/route.js
+++ src/app/api/portfolio/route.js
@@ -2,11 +2,10 @@ import { NextResponse } from 'next/server';
 import Portfolio from 'src/models/Portfolio';
 import connect from 'src/utils/db';
 
-export const GET = async (request, { params }) => {
-  const { category } = params;
+export const GET = async (request) => {
   try {
     await connect();
-    const portfolio = await Portfolio.findOne({ category: category });
+    const portfolio = await Portfolio.find();
     return new NextResponse(JSON.stringify(portfolio), { status: 200 });
   } catch (error) {
     return new NextResponse('Database connection error', { status: 500 });
